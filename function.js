4f12474c5b6f8a6b0cef6f355e8f7c8c8d6b2e20
rohithavijay2104-pixel <rohithavijay2104@gmail.com>
2026-06-12 15:22:12 +0530

    added function.js

---
+++ function.js
@@ -0,0 +1,12 @@
+//non parametric functions
+function greet(){
+    console.log("say hello..!");
+}
+greet();
+
+//parametric functions
+function add(a,b){
+    console.log(a+b);
+    return a+b;
+}
+add(5,3);
