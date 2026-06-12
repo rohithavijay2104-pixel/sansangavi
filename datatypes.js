7def5a494436d28a0d87064606e16898d039ff30
rohithavijay2104-pixel <rohithavijay2104@gmail.com>
2026-06-12 15:11:22 +0530

    added datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,50 @@
+console.log("welcome to primitive datatypes");
+
+//number
+console.log("datatype number");
+var age = 30;
+console.log(age);
+
+//string
+console.log("datatypes string");
+ var name = "joy";
+ console.log(name);
+
+//boolean
+ console.log("data types boolean");
+ var bool = "true";
+ console.log(bool);
+
+//BigInt
+ console.log("data types bigInt");
+ var big = "133324455678";
+ console.log(big);
+
+//null
+ console.log("data types null");
+ var isstudent ="true";
+ console.log(isstudent);
+
+//symbols
+ console.log("data types symbols");
+ var symbolsvalue = ('symbols');
+ console.log(symbolsvalue);
+
+ //undefined
+    console.log("data types undefined");
+    var undefined ;
+    console.log(undefined )
+
+    //non-primitive
+    console.log("objects");
+    var person={
+        name:"help",
+        age:5
+    };
+    console.log(person,age);
+
+    //Array
+    console.log("Array");
+    var number =["kiwi","apple","pine apple","grapes","fig"];
+    console.log(number.length);
+    console.log(number[3]);
--- oper.js
+++
@@ -1,4 +0,0 @@
-var a =30;
-console.log(a>=10);
-console.log(a<=20);
-console.log(a==30);
