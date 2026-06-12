92f633a9c8cbacb95473335fc6aa2950e8be771d
rohithavijay2104-pixel <rohithavijay2104@gmail.com>
2026-06-12 15:51:36 +0530

    added conditional.js

---
+++ conditional.js
@@ -0,0 +1,15 @@
+var light = "red"
+
+if(light =="green"){
+    console.log("go!");
+}
+else if(light=="yellow"){
+    console.log("wait!");
+}
+else if(light=="red"){
+    console.log("stop!");
+}
+else{
+    console.log("invalid light color");
+}
+console.log("outside if");
