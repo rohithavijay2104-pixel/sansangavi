0b07194c34d252328dbe5c529c05e037a535a017
rohithavijay2104-pixel <rohithavijay2104@gmail.com>
2026-06-13 10:58:30 +0530

    added exercise.js

---
+++ exercise.js
@@ -0,0 +1,20 @@
+for(let i = 1;i <=10;i++){
+    console.log(i + "x 2="+(i* 2))
+}
+
+let students = [
+    "arunthathi",
+    "sangavi",
+    "ramya",
+    "rohitha"
+];
+for(let j=0;j< students.length;j++){
+    console.log(students[j]);
+}
+
+let k=1;
+ 
+while(k<=10){
+    console.log(k);
+    (k++);
+}
